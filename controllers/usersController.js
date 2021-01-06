f9ea0703c826f56068fa992901a8d247444ffb33
essiva <essimaria.varjonen@gmail.com>
2021-01-06 22:31:42 +0200

    more comments and created flash messages in register page

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,27 +2,32 @@ const User = require("../models/user");
 
 module.exports = {
 
-     //rekisteröityminen
-     new: (req, res) => {
+     //register
+     new: (req, res) => {//takes incoming requests to create a new user and render the form in register.ejs
         res.render("users/register")
     },
 
-    create: (req, res, next) => {
+    create: (req, res, next) => {//creates new user from data that comes from register.ejs form
         let userParams = {
             userName: req.body.userName,
             email: req.body.email,
             password: req.body.password
         }
 
-        User.create(userParams)
+        User.create(userParams)//passing those parameters
             .then(user => {
-                res.locals.redirect = "/";
-                res.locals.user = user;
+                req.flash("success", `${user.userName}'s account created successfully!`);
+                res.locals.redirect = "/";//redirects new user to home page
+                res.locals.user = user;//finds if there already exists this user
                 next();
             })
             .catch(error => {
                 console.log(`Error ${error.message}`);
-            })
+                res.locals.redirect = "/register";
+                req.flash(
+                    "error", "Failed to create user account. This user already exists!");
+                    next();
+            });
     },
     redirectView: (req, res, next) => {
         let redirectPath = res.locals.redirect;
@@ -32,7 +37,7 @@ module.exports = {
         next();
     },
 
-    //kirjautuminen
+    //login
     login: (req,res,next) => {
         res.render("users/login");
     }
--- main.js
+++ main.js
@@ -16,6 +16,26 @@ const express = require("express"),
       connectFlash = require("connect-flash"),
       expressValidator = require("express-validator");
 
+router.use(cookieParser("secret_passcode"));//using cookie-parser as middleware and to use secret passcode
+router.use(expressSession ({//configure express-session to use cookie-parser
+    secret: "secret_passcode",//provide a secret key to encrypt session data
+    cookie: {
+        maxAge: 40000
+    },
+    resave: false,//don't want to update existing session data on the server if nothing has changed in the existing session
+    saveUninitialized: false//don't want to send a cookie to the user if no messages are added to the session
+}));
+router.use(connectFlash());//use connect-flash as middleware
+
+router.use((req,res,next) => {
+    res.locals.flashMessages = req.flash();//pass a local object called flashMessages to the view
+    next();
+});
+
+const methodOverride = require("method-override");//Lets use HTTP verbs PUT or DELETE. Use methodOverride as middleware
+router.use(methodOverride("_method", {
+    methods: ["POST", "GET"]
+}));
 
 mongoose.Promise = global.Promise;//using promises
 
@@ -29,24 +49,25 @@ mongoose.set("useCreateIndex", true);
 const db = mongoose.connection;//create connection
 
 db.once("open", () => {
-    console.log("Successfull connection to database!")//appears on the console when the database starts successfully
+    console.log("Successfull connection to database!")//appears on the console when the database connection is success
 });
 
 app.set("port", process.env.PORT || 3000);
-app.set("view engine", "ejs");//setting view engine and ejs
+app.set("view engine", "ejs");//setting ejs
 
 router.use(express.static("public"));//using files from public folder
-router.use(layouts);//using layouts
+router.use(layouts);//using layout module
 router.use(
-    express.urlencoded({
+    express.urlencoded({//telling the Express.js to use body-parser for processing URL-encoded and JSON parameters
         extended: false
     })
 );
-router.use(express.json());
+router.use(express.json());//analyzing data within incoming requests
 
 
-router.get("/", homeController.index);
+router.get("/", homeController.index);//route for home page
 
+//routes for the register and login pages
 router.get("/register", usersController.new);
 router.post("/create", usersController.create, usersController.redirectView);
 router.get("/login", usersController.login);
